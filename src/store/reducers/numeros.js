48a733703dc565263b4693ccad8fb61268a16829
denilsondonolato <42578438+DenilsonDonolato@users.noreply.github.com>
2021-01-17 08:12:56 -0300

    refactor reducer

---
+++ src/store/reducers/numeros.js
@@ -0,0 +1,21 @@
+const initialState = {
+  min:1,
+  max:10
+}
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'NUM_MIN_CHANGE':
+      return {
+        ...state,
+        min: action.payload
+      }
+    case 'NUM_MAX_CHANGE':
+      return {
+        ...state,
+        max: action.payload
+      }    
+    default:
+      return state
+  }
+}
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,25 +1,9 @@
 import { createStore, combineReducers} from 'redux';
 
+import numerosReducer from "./reducers/numeros";
+
 const reducers = combineReducers({
-  numeros: function(state, action) {
-    switch (action.type) {
-      case 'NUM_MIN_CHANGE':
-        return {
-          ...state,
-          min: action.payload
-        }
-      case 'NUM_MAX_CHANGE':
-        return {
-          ...state,
-          max: action.payload
-        }    
-      default:
-        return {
-          min: 1,
-          max: 10
-        }
-    }
-  }
+  numeros: numerosReducer
 });
 
 export default function storeConfig() {
